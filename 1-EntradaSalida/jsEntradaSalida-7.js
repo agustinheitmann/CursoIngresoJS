a1596925e4b29672037ce8dea17542e581785c3c
agustinheitmann <52494266+agustinheitmann@users.noreply.github.com>
2019-07-04 10:09:50 -0300

    ej 6 y 7

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,17 +4,55 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+    var numero1; var numero2; 
+
+    numero1=document.getElementById('numeroUno').value;
+    numero1=parseInt(numero1);
+    
+    numero2=document.getElementById('numeroDos').value;
+    numero2=parseInt(numero2);
+    
+    var suma; 
+
+    suma= numero1 + numero2;
+
+    alert("la suma es "+suma)
+
+    
 }
 
 function restar()
 {
-	
+	  var numero1; var numero2; 
+
+    numero1=document.getElementById('numeroUno').value;
+    numero1=parseInt(numero1);
+    
+    numero2=document.getElementById('numeroDos').value;
+    numero2=parseInt(numero2);
+    
+    var resta; 
+
+    resta= numero1 - numero2;
+
+    alert("la resta es "+resta)
 }
 
 function multiplicar()
 { 
-	
+	  var numero1; var numero2; 
+
+    numero1=document.getElementById('numeroUno').value;
+    numero1=parseInt(numero1);
+    
+    numero2=document.getElementById('numeroDos').value;
+    numero2=parseInt(numero2);
+    
+    var dividir; 
+
+    dividir= numero1 / numero2;
+
+    alert("la division es "+dividir)
 }
 
 function dividir()
