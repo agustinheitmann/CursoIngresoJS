aa3c90727bdfb39e7ea3d5a2aab3caea5740b181
agustinheitmann <52494266+agustinheitmann@users.noreply.github.com>
2019-07-10 12:20:50 -0300

    Ej 1 / 3

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -2,7 +2,18 @@ function mostrar()
 {
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
+switch(mesDelAño)
+{
+    case "Enero": {alert("Que comiences bien el año");break};
+    case "Marzo": {alert("a clases!!");break};
+    case "Julio": {alert("Vacaciones!");break};
+    case "Diciembre": {alert("Fiestas!!");break}; 
 
+
+
+
+
+}
 
 
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -3,7 +3,16 @@ function mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-//alert (mesDelAño);
+switch(mesDelAño)
+    {
+        case "Febrero": {alert("Este mes tiene 29 dias");break};
+        
+        default:
+            {alert("Este mes tiene 30 dias");break};     
+
+
+
+    }
 	
 	
 
