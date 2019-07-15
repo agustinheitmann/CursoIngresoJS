5bf01a53f3667bd0af772f11697604a08771a7ce
agustinheitmann <52494266+agustinheitmann@users.noreply.github.com>
2019-07-14 21:27:30 -0300

    cuatro

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,22 @@
 function mostrar()
 {
+   var numero1=prompt("Numero uno es ");
+   var numero2=prompt("Numero dos es ");
+   var resultado;
 
+    if(numero1 == numero2) {resultado=numero1 + numero2;}
+
+    else{
+
+        if (numero1>numero2){resultado=numero1/numero2;}
+
+        else{resultado=+numero1 + +numero2;}
+
+    }
+
+    if(resultado<50) {resultado="La suma "+resultado +" es resultado es menor a 50"}
+
+
+
+   alert(resultado)
 }
