8ebe402cf85dd126a7fd92397870b084b7d4c811
agustinheitmann <52494266+agustinheitmann@users.noreply.github.com>
2019-07-16 18:59:28 -0300

    tres

--- 9-Parciales/tres.js
+++ 9-Parciales/tres.js
@@ -1,10 +1,11 @@
 function mostrar()
 {
-    var precio =prompt("El precio es");parseInt(precio)
+    var precio =prompt("El precio es");parseInt(precio);
     var porcentaje =prompt("El porcentaje es");parseInt(porcentaje);
 
     var resultado = precio * porcentaje/100;
+    resultado= precio- resultado; 
 
     document.getElementById('elPrecioFinal').value=resultado;
-
 }
+
