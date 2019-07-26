29e706133da9ad2bb438f195666157afd4b37b29
agustinheitmann <52494266+agustinheitmann@users.noreply.github.com>
2019-07-26 19:11:57 -0300

    Siete

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,37 +1,62 @@
 function mostrar()
 {
-    var alumnos=0;
+  
     var notas=0;
-	var sexo;
-	var promedio;
-    var notabaja;
+    var alumnos=0;
+    var sexo;
+    var promedio;
+    var notabaja=10;
+    var contador=0;
+    var sexoNotaBaja;
+    var sumanotas=0;
 
     while(alumnos<5)
-    
-    {   
-        notas = prompt("ingrese un número entre 0 y 10.");
-        notas = parseInt(notas);
-        sexo = prompt("ingrese f o m");
-       
 
-        while(isNaN(notas))
         {
-            notas=prompt("El numero es invalido, ingrese un numero valido");
-        }
+            notas=prompt("Ingrese un numero del 1 al 10")
+            notas=parseInt(notas);
+
+            while(isNaN(notas)||notas<0||notas>10)
+                
+                {
+                    notas=prompt("Incorrecto, ingrese un numero del 1 al 10")
+                    notas=parseInt(notas);
+                }
+            
+            sexo=prompt("Ingrese f o m");
+            sexo=sexo.toLowerCase();
+
+            while(sexo!="f" && sexo!="m")
+                
+                {
+                    sexo=prompt("El sexo es invalido, ingrese sexo f o m");
+                    sexo=sexo.toLowerCase();
+                }
         
-        while (sexo!="f" && sexo !="m")
+            if(notas<notabaja)
+                
+                {
+                    notabaja=notas;
+                    sexoNotaBaja=sexo;
+                }
+            
+            if(sexo=="m" && notas>=6)
+                
+                {
+                    contador++;
+                }
 
-        {
-            sexo=prompt("El sexo es invalido;Ingrese un sexo verdadero")
-        }       
+            alumnos++;
+            sumanotas+=notas;
+            promedio=sumanotas/5;
+
+        
+        }
+
+        alert("Promedio: "+promedio);
+        alert("Nota baja: "+notabaja+" Sexo: "+sexoNotaBaja);
+        alert("Varones con nota mayor o igual a 6: "+contador);
     
-        alumnos++;
-        notas+=sexo;
-        promedio=notas/5;
-       
-
-    }   
-
 
 
 }
