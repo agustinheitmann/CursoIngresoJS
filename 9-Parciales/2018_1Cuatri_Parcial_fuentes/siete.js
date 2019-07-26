bb9678f53bf259555e175ea5b9a31dc4a90ebf7c
agustinheitmann <52494266+agustinheitmann@users.noreply.github.com>
2019-07-26 00:30:27 -0300

    Hay que hacerlo bien

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,37 @@
 function mostrar()
 {
+    var alumnos=0;
+    var notas=0;
+	var sexo;
+	var promedio;
+    var notabaja;
+
+    while(alumnos<5)
+    
+    {   
+        notas = prompt("ingrese un número entre 0 y 10.");
+        notas = parseInt(notas);
+        sexo = prompt("ingrese f o m");
+       
+
+        while(isNaN(notas))
+        {
+            notas=prompt("El numero es invalido, ingrese un numero valido");
+        }
+        
+        while (sexo!="f" && sexo !="m")
+
+        {
+            sexo=prompt("El sexo es invalido;Ingrese un sexo verdadero")
+        }       
+    
+        alumnos++;
+        notas+=sexo;
+        promedio=notas/5;
+       
+
+    }   
+
+
 
 }
