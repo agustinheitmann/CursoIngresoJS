f6af9734832e265ded3887fe60630dd629dedeb1
agustinheitmann <52494266+agustinheitmann@users.noreply.github.com>
2019-08-03 23:54:18 -0300

    8-9

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,4 +1,88 @@
 function mostrar()
 {
+    var marca;
+    var peso=0;
+    var temperatura=0;
+    var respuesta="si";
+    var pesomaximo=0;
+    var pesogeneral=0;
+    var marcamaxima;
+    var cantceros=0;
+    var maximo=0;
+    var minimo=100;
+    var cantpeso=0;
+    var temperaturapar=0;
 
-}
+        while(respuesta=="si")
+        {
+            marca=prompt("Ingrese la marca del producto");
+
+            peso=prompt("Ingrese el peso del producto");
+            peso=parseInt(peso);
+            
+            while(isNaN(peso)||peso<0||peso>100)
+            {
+                peso=prompt("Ingrese un peso valido");
+                peso=parseInt(peso);
+            }
+
+            temperatura=prompt("Ingrese temperatura");
+            temperatura=parseInt(temperatura);
+
+            while(isNaN(temperatura)||temperatura<-31 || temperatura>31)
+            {
+                temperatura=prompt("Ingrese una temperatura valida");
+                temperatura=parseInt(temperatura);
+            }   
+
+
+            if(temperatura%2=="0")
+            {
+                temperaturapar++;
+            }
+
+            if(peso>pesomaximo)
+            {
+                pesomaximo=peso;
+                marcamaxima=marca;
+
+            }
+            
+            if(temperatura<"0")
+            {
+                cantceros++;
+            }
+
+            if(peso>0)
+            {
+                pesogeneral+=peso;
+                cantpeso++;
+                promedio=pesogeneral/cantpeso;
+            }
+
+            if(peso>maximo)
+            {
+                maximo=peso;
+            }
+            
+            if(peso<minimo)
+        
+            {
+                minimo=peso;
+            }
+       
+            respuesta=prompt("Desea ingresar otra marca?");
+            respuesta=prompt("Desea ingresar otro peso?");
+            respuesta=prompt("Desea ingresar otra temperatura?");
+        }
+
+            document.write("La cantidad de temperaturas pares es "+temperaturapar+ "<br>");
+            document.write("La marca del producto más pesado "+marcamaxima+ "<br>");
+            document.write(" La cantidad de productos que se conservan a menos de 0 grados." +cantceros+ "<br>");
+            document.write("El promedio del peso de todos los productos. "+promedio+ "<br>");
+            document.write("El peso máximo "+maximo+ "<br>");
+            document.write("El peso minimo  "+minimo+ "<br>");
+
+
+
+    }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,4 +1,98 @@
 function mostrar()
 {
+    var letra;
+    var numero;
+    var numpar=0;
+    var numinpar=0;
+    var cantceros=0;
+    var promediopositivos=0;
+    var numeronegativo=0;
+    var sumanegativos=0;
+    var cantpositivos=0;
+    var cantnegativos=0;
+    var numeromax=0;
+    var letramax;
+    var letramin;
+    var numeromin=100;
+    var respuesta="si";
+    var sumanumeros=0;
+    var contador=0;
+
+        while(respuesta=="si")
+
+            {
+                
+                letra=prompt("Ingrese un letra");
+                
+                while(letra.length!=1)
+                    {
+                        letra=prompt("Ingrese un solo digito");
+                    }
+
+                numero=prompt("Ingrese un numero del -100 al 100");
+                numero=parseInt(numero);
+                
+                    while(isNaN(numero) || numero<=-100 || numero>=100)
+                    {
+                        numero=prompt("Ingrese un numero valido");
+                        numero=parseInt(numero);
+                    }
+            
+                if(numero%2==0)
+                    {
+                        numpar++;
+                    }
+                else
+                    {
+                        numinpar++;
+                    }
+                
+                if(numero=="0")
+                    {
+                        cantceros++;
+                    }
+
+                if(numero>0)
+                    {
+                        sumanumeros+=numero;
+                        cantpositivos++;
+                        promediopositivos=sumanumeros/cantpositivos;
+                    }
+
+                else
+                    {
+                        sumanegativos+=numero;
+                        cantnegativos++
+                    }
+
+                if(numero>numeromax)
+                    {
+                        numeromax=numero;
+                        letramax=letra;
+                    }
+                
+                else
+                {
+                    if(numero<numeromin)
+                    {
+                        numeromin=numero;
+                        letramin=letra;
+                    }
+                }
+
+                promediopositivos=sumanumeros/cantpositivos;
+
+
+                respuesta=prompt("Desea ingresar otra letra?");
+                respuesta=prompt("Desea ingresar otro numero?");
+            }
+
+            document.write("La cantidad de numeros pares es "+numpar + "<br>");
+            document.write("La cantidad de numeros impares es " +numinpar + "<br>") ;
+            document.write("La cantidad de ceros es "+cantceros + "<br>");
+            document.write("El promedio de todo los positivos es "+promediopositivos+ "<br>");
+            document.write("La suma de todos los negativos es "+cantnegativos+ "<br>");
+            document.write("El numero maximo es "+numeromax+ "y su letra es "+letramax+ "<br>");
+            document.write("El numero minimo es "+numeromin+ "y su letra es "+letramin+ "<br>")
 
 }
